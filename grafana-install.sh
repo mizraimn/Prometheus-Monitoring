c734d076313650e316cad1552fbf0badb8ac2263
Mizraim <mizraimnuno@gmail.com>
2022-09-03 10:11:09 -0500

    File upload

---
+++ grafana-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
+sudo dpkg -i grafana_7.3.4_amd64.deb
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
